a05c74eeedc5c77a5efab7c9c8c4d61266c26605
Arjan de Ruijter <rra@mboutrecht.nl>
2014-10-02 09:28:04 +0200

    Random toegevoegd

--- 9-Decision_Making/Program.cs
+++ 9-Decision_Making/Program.cs
@@ -9,6 +9,19 @@ namespace _9_Decision_Making
     {
         static void Main(string[] args)
         {
+            /* Maak een raadspelletje waarbij de speler een getal moet raden onder de 25.
+             * je mag natuurlijk ook een ander getal nemen.
+             * Als het getal niet correct is geraden mag de persoon het nog een beperkt aantal
+             * keren proberen. Na dit aantal stopt het spel.
+             */
+
+            Random random = new Random();
+            double willekeurigGetal = random.NextDouble() * 25;
+            short afgerondGetal = (short)Math.Round(willekeurigGetal);
+            Console.WriteLine("Dit is het random getal: {0}", afgerondGetal.ToString());
+
+
+            
             Console.ReadLine();
         }
     }
