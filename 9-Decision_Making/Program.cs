3bf30540720a2c2d644aefb5f89e81e0ff269830
Arjan de Ruijter <rra@mboutrecht.nl>
2014-10-02 13:50:37 +0200

    Spel drie maal raden laatste versie

--- 9-Decision_Making/Program.cs
+++ 9-Decision_Making/Program.cs
@@ -14,11 +14,74 @@ namespace _9_Decision_Making
              * Als het getal niet correct is geraden mag de persoon het nog een beperkt aantal
              * keren proberen. Na dit aantal stopt het spel.
              */
+            short teRadenGetal = 13;
+            short getalVanSpeler;
 
             Random random = new Random();
             double willekeurigGetal = random.NextDouble() * 25;
             short afgerondGetal = (short)Math.Round(willekeurigGetal);
-            Console.WriteLine("Dit is het random getal: {0}", afgerondGetal.ToString());
+
+            // Vraag om invoer
+            Console.WriteLine("Geef een getal tussen de 0 en 25\nu heeft 3 kansen om te winnen\n");
+
+            Console.Write("Uw eerste getal: ");
+            
+            // Stop de ingevoerde waarde in een string genaamd input
+            string input  =  Console.ReadLine();
+
+            // Cast de string naar een short variabele
+            getalVanSpeler = Convert.ToInt16(input);
+
+            if (getalVanSpeler != teRadenGetal)
+            {
+                Console.WriteLine("++++++++++++++++++++++++");
+                Console.WriteLine("U heeft het getal niet goed geraden.\nU heeft {0} ingevoerd," +
+                                  "\nGeef opnieuw een getal tussen de 0 en de 25\n", getalVanSpeler);
+                
+                Console.Write("Uw tweede getal: ");
+
+                // Stop de ingevoerde waarde in een string genaamd input
+                input  =  Console.ReadLine();
+
+                // Cast de string naar een short variabele
+                getalVanSpeler = Convert.ToInt16(input);
+
+                if (getalVanSpeler != teRadenGetal)
+                {
+                    Console.WriteLine("++++++++++++++++++++++++");
+                    Console.WriteLine("U heeft het getal niet goed geraden.\nU heeft {0} ingevoerd," +
+                                      "\nGeef opnieuw een getal tussen de 0 en de 25\n", getalVanSpeler);
+
+                    Console.Write("Uw derde en laatste getal: ");
+
+                    // Stop de ingevoerde waarde in een string genaamd input
+                    input = Console.ReadLine();
+
+                    // Cast de string naar een short variabele
+                    getalVanSpeler = Convert.ToInt16(input);
+
+                    if (getalVanSpeler != teRadenGetal)
+                    {
+                        Console.WriteLine("++++++++++++++++++++++++++");
+                        Console.WriteLine("U heeft het getal de derde keer niet goed geraden.\nU heeft {0} ingevoerd," +
+                                          "We stoppen het spel.\nHet getal was: {1}", getalVanSpeler, teRadenGetal);
+                        // Stop de ingevoerde waarde in een string genaamd input
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gefeliciteerd u heeft het getal {0} in drie keer geraden", teRadenGetal);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Gefeliciteerd u heeft het getal {0} in twee keer geraden", teRadenGetal);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Gefeliciteerd u heeft het getal {0} in een keer geraden", teRadenGetal);
+            }
+           
 
 
             
